7ae59e552626e6dc7c14adf5dcea04147db7b54f
uras <uras@i.ua>
2016-07-13 22:58:32 +0300

    Calc without spring

---
+++ src/main/java/Dividing.java
@@ -0,0 +1,13 @@
+import java.math.BigDecimal;
+
+public class Dividing implements Operation {
+    @Override
+    public BigDecimal count(BigDecimal bigDecimal, BigDecimal bigDecimal1) {
+        return bigDecimal.divide(bigDecimal1);
+    }
+
+    @Override
+    public char getOperator() {
+        return '/';
+    }
+}
---
+++ src/main/java/Multiplying.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+public class Multiplying implements Operation {
+
+    @Override
+    public BigDecimal count(BigDecimal v, BigDecimal v1) {
+        return v.multiply(v1);
+    }
+
+    @Override
+    public char getOperator() {
+        return '*';
+    }
+}
